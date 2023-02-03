6cf0c44c4d14458137d87dfe4791a1f569e2d1dc
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-02-03 15:23:40 +0100

    Refactor commit + changed confetti effect

--- js/main.js
+++ js/main.js
@@ -34,7 +34,29 @@ function countdownCounter() {
 
     // Se il countdown è arrivato a 0
     if (countdownDistance <= 0) {
+        // Ferma il countdown
         clearInterval(clockCountdown);
-        confetti({ particleCount: 100, spread: 160 });
+
+        // Confetti Firework Effect
+        const duration = 5 * 1000;
+        const animationEnd = Date.now() + duration;
+        const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+        function randomInRange(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        const interval = setInterval(function () {
+            const timeLeft = animationEnd - Date.now();
+
+            if (timeLeft <= 0) {
+                return clearInterval(interval);
+            }
+
+            const particleCount = 50 * (timeLeft / duration);
+            // since particles fall down, start a bit higher than random
+            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+        }, 250);
     }
 }
